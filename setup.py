c61e6cf635541e40dbc60d1c551dbd4cc81dbfa0
Rairye <37942271+Rairye@users.noreply.github.com>
2021-10-09 14:42:06 +0900

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'zh_sentence',  
+  packages = ['zh_sentence'], 
+  version = '0.0.1', 
+  license='apache-2.0', 
+  description = 'Light-weight sentence tokenizer for Chinese languages.',
+  author = 'Rairye', 
+  url = 'https://github.com/Rairye/zh-sentence',   
+  download_url = 'https://github.com/Rairye/zh-sentence/archive/refs/tags/v0.0.1.tar.gz',
+  keywords = ['Chinese', 'Sentence', 'Tokenizer'],
+
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: End Users/Desktop',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License', 
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.1',
+    'Programming Language :: Python :: 3.2',
+    'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+  ],
+)
